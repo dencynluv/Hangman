c8f875399dc593a227708d7757adc51c63a56277
Cynthia Soto <sotocyn@gmail.com>
2017-02-18 18:51:18 -0800

    Version 1. Works! but there is a conditional statement causing problems. Condition is not met when there are repeated letters in a word.

--- hangman.py
+++ hangman.py
@@ -16,4 +16,46 @@ words = r.text.split()
 # for word in words:
 #     print word
 
-secret_word = random.choice(words)
+
+while True:
+    start = raw_input("Press enter/return to start, or Q to quit ")
+    if start.lower() == 'q':
+        break
+
+    secret_word = random.choice(words)
+    bad_guesses = []
+    good_guesses = []
+
+    while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
+        for letter in secret_word:
+            if letter in good_guesses:
+                print letter,
+            else:
+                print '_',
+
+        print('')
+        print('Strikes: {}/7'.format(len(bad_guesses)))
+        print('')
+
+        guess = raw_input("Guess a letter: ").lower()
+
+        if len(guess) != 1:
+            print("You can only guess a single letter!")
+            continue
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guessed that letter!")
+            continue
+        elif not guess.isalpha():
+            print("You can only guess letters!")
+            continue
+
+        if guess in secret_word:
+            good_guesses.append(guess)
+            if len(good_guesses) == len(list(secret_word)):
+                print("You win! The word was {}".format(secret_word))
+                break
+        else:
+            bad_guesses.append(guess)
+    else:
+        print("You didn't guess it! My word was {}".format(secret_word))
+
